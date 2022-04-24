1908fbd71adbb01aba47b851ea67281429b28588
bobmi1 <87269004+bobmi1@users.noreply.github.com>
2022-04-24 12:51:23 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+        function bop1() {
+            window.location = "index.html";
+        }
+        
+        function bop2() {
+            window.location = "index2.html";
+        }
+        
+        function bop3() {
+            window.location = "index3.html";
+        }
+        
+        function bop4() {
+            window.location = "index4.html";
+        }
+        
+        function bop5() {
+            window.location = "index5.html";
+        }
+        
